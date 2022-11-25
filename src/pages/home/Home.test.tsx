9aefa7f6802a68a4293f3a0c72b40b43c673c1d2
NevenaGazivoda <nevena.g@hotmail.com>
2022-11-25 21:32:16 +0100

    Git configured

---
+++ src/pages/home/Home.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AtpHome from './Home';
+
+test('renders home', () => {
+  render(<AtpHome articles={[]} />);
+});
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import AtpArticleCard from '../../components/article-card/AtpArticleCard';
+import AtpText from '../../components/text/AtpText';
+import { Article } from '../../models/article.model';
+import './Home.scss';
+
+type Props = {
+  articles: Article[];
+};
+
+export const AtpHome: FC<Props> = ({ articles }) => {
+  return (
+    <div className="atp-page atp-home">
+      <AtpText>ARTICLES</AtpText>
+      <div className="atp-recently-viewed__articles">
+        {articles.map((card, cardIndex) => (
+          <AtpArticleCard key={cardIndex} article={card} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AtpHome;
