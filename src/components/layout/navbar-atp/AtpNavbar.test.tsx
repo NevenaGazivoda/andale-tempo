420361cb5815901a2118e37162ee971186ebc122
NevenaGazivoda <nevena.g@hotmail.com>
2022-12-12 15:27:10 +0100

    Resolving conflict

--- src/components/layout/navbar-atp/AtpNavbar.test.tsx
+++ src/components/layout/navbar-atp/AtpNavbar.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import AtpNavbar from './AtpNavbar';
 import { MemoryRouter } from 'react-router-dom';
-import ShoppingCartStore from '../../utilities/ShoppingCartStore';
+import ShoppingCartStore from 'utilities/ShoppingCartStore';
 
 test('renders navbar', () => {
   render(
